fac638325fbbdb7ea94be91c072df78d5cc26a4e
karish-123 <113624476+karish-123@users.noreply.github.com>
2025-04-15 20:21:45 +0530

    Initial Commit

---
+++ ray_tracer/ray_tracerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ray_tracerApp.swift
+//  ray_tracer
+//
+//  Created by KARISHNII KUBER on 15/04/25.
+//
+
+import SwiftUI
+
+@main
+struct ray_tracerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ray_tracerTests/ray_tracerTests.swift
@@ -0,0 +1,17 @@
+//
+//  ray_tracerTests.swift
+//  ray_tracerTests
+//
+//  Created by KARISHNII KUBER on 15/04/25.
+//
+
+import Testing
+@testable import ray_tracer
+
+struct ray_tracerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
